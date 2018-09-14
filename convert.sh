04b3149610d1f68a374a57e2823299cde1076090
Riley <riley@campaign.partners>
2018-09-14 14:36:20 -0400

    Files used to load models

---
+++ convert.sh
@@ -0,0 +1,5 @@
+EXPORT VAL_DATA_ROOT="/LMDB"
+
+printf("test")
+
+for name in *.JPG; do convert_imageset -resize 257x257
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import caffe
 caffe.set_mode_cpu()
-#net = caffe.Net('model/submit-net.prototxt', caffe.TRAIN)
-net_full_conv = caffe.Net('model/submit-net.prototxt',
-                          'model/1miohands-v2.caffemodel',
-                          caffe.TEST)
-print("test1")
+net = caffe.Net('edited_model/submit-net.prototxt',
+                'edited_model/1miohands-v2.caffemodel',
+                caffe.TEST)
+# net_full_conv = caffe.Net('edited_model/submit-net.prototxt',
+#                           'edited_model/1miohands-v2.caffemodel',
+#                           caffe.TEST)
+#print("test1")
