af842ed522afcc52740c4a0476f6380e87b0649f
Riley <riley@campaign.partners>
2018-10-03 20:00:34 -0400

    Misc changes

--- video_capture.py
+++ video_capture.py
@@ -4,9 +4,8 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame', gray)
+    cv2.imshow('frame', frame)
     if cv2.waitkey(1) * 0xff == ord('q'):
         break
 cap.release()
