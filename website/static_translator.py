9f9756a6e60f129125899fa52128431e61f1c5ff
Saarthakm <saarthak.sunny@gmail.com>
2018-10-29 20:40:35 -0400

    Latest website updates

--- website/static_translator.py
+++ website/static_translator.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def capture_video():
-    return send_from_directory('','capture_video.html')
+    return send_from_directory('','test_video.html')
+
     #return "test"
-    
+
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
